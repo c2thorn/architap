f7de48e3c48cede9d04a791122dd89d573d6739e
Cameron <c2thorn@gmail.com>
2018-07-02 20:10:58 -0500

    update

---
+++ Assets/tabScroll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tabScroll : MonoBehaviour {
+
+	public ScrollRect scrollrect;
+	public Image scrollArea;
+	public GameObject scrollBar;
+	// Use this for initialization
+	void Start () {
+		disable();
+	}
+
+	public void enable() {
+		scrollrect.horizontal = true;
+		scrollBar.active = true;
+		scrollArea.enabled = true;
+	}
+
+	public void disable() {
+		scrollrect.horizontal = false;
+		scrollBar.active = false;
+		scrollArea.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
