f354f622746a2794b395877350cc7b9be1991095
Aaron Brandon <>
2018-09-12 21:02:50 -0500

    particles?

---
+++ Assets/dustParticle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dustParticle: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().Play();
+		StartCoroutine(endDust());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	IEnumerator endDust() {
+		yield return new WaitForSeconds(1.0f);
+		Destroy(gameObject);
+    }
+}
